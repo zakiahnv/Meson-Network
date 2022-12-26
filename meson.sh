578fe4157c17cfd1dfe8449b72bb6bcec18ae5fe
zakiahnp <87823344+zakiahnv@users.noreply.github.com>
2022-12-26 19:07:37 +0700

    Update meson.sh

--- meson.sh
+++ meson.sh
@@ -1,3 +1,4 @@
+sudo apt update && sudo ufw allow 443 && sudo ufw enable
 wget 'https://staticassets.meson.network/public/meson_cdn/v3.1.18/meson_cdn-linux-amd64.tar.gz' && tar -zxf meson_cdn-linux-amd64.tar.gz && rm -f meson_cdn-linux-amd64.tar.gz && cd ./meson_cdn-linux-amd64 && sudo ./service install meson_cdn
 sudo ./meson_cdn config set --token=gjphcmgltqngadsjplyuzgpu --https_port=443 --cache.size=30 && sudo ./service restart meson_cdn
 sudo ./service start meson_cdn
